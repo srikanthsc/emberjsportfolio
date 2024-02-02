d80f7e45be258d9ebfa49d0db48f86a5452f5db2
unknown <srikanthcollaty92@gmail.com>
2024-02-02 18:00:09 +0100

    Updates

---
+++ assets/chunk.143.836b8b0e0b7f37e236eb.js
@@ -0,0 +1,53 @@
+/*! For license information please see chunk.143.836b8b0e0b7f37e236eb.js.LICENSE.txt */
+var __ember_auto_import__;(()=>{var t={863:(t,e,i)=>{"use strict"
+i.r(e),i.d(e,{clearAllBodyScrollLocks:()=>_,disableBodyScroll:()=>m,enableBodyScroll:()=>p})
+var n=!1
+if("undefined"!=typeof window){var o={get passive(){n=!0}}
+window.addEventListener("testPassive",null,o),window.removeEventListener("testPassive",null,o)}var r="undefined"!=typeof window&&window.navigator&&window.navigator.platform&&(/iP(ad|hone|od)/.test(window.navigator.platform)||"MacIntel"===window.navigator.platform&&window.navigator.maxTouchPoints>1),s=[],a=!1,l=-1,c=void 0,u=void 0,h=function(t){return s.some((function(e){return!(!e.options.allowTouchMove||!e.options.allowTouchMove(t))}))},d=function(t){var e=t||window.event
+return!!h(e.target)||e.touches.length>1||(e.preventDefault&&e.preventDefault(),!1)},f=function(){void 0!==u&&(document.body.style.paddingRight=u,u=void 0),void 0!==c&&(document.body.style.overflow=c,c=void 0)},m=function(t,e){if(t){if(!s.some((function(e){return e.targetElement===t}))){var i={targetElement:t,options:e||{}}
+s=[].concat(function(t){if(Array.isArray(t)){for(var e=0,i=Array(t.length);e<t.length;e++)i[e]=t[e]
+return i}return Array.from(t)}(s),[i]),r?(t.ontouchstart=function(t){1===t.targetTouches.length&&(l=t.targetTouches[0].clientY)},t.ontouchmove=function(e){1===e.targetTouches.length&&function(t,e){var i=t.targetTouches[0].clientY-l
+!h(t.target)&&(e&&0===e.scrollTop&&i>0||function(t){return!!t&&t.scrollHeight-t.scrollTop<=t.clientHeight}(e)&&i<0?d(t):t.stopPropagation())}(e,t)},a||(document.addEventListener("touchmove",d,n?{passive:!1}:void 0),a=!0)):function(t){if(void 0===u){var e=!!t&&!0===t.reserveScrollBarGap,i=window.innerWidth-document.documentElement.clientWidth
+e&&i>0&&(u=document.body.style.paddingRight,document.body.style.paddingRight=i+"px")}void 0===c&&(c=document.body.style.overflow,document.body.style.overflow="hidden")}(e)}}else console.error("disableBodyScroll unsuccessful - targetElement must be provided when calling disableBodyScroll on IOS devices.")},_=function(){r?(s.forEach((function(t){t.targetElement.ontouchstart=null,t.targetElement.ontouchmove=null})),a&&(document.removeEventListener("touchmove",d,n?{passive:!1}:void 0),a=!1),l=-1):f(),s=[]},p=function(t){t?(s=s.filter((function(e){return e.targetElement!==t})),r?(t.ontouchstart=null,t.ontouchmove=null,a&&0===s.length&&(document.removeEventListener("touchmove",d,n?{passive:!1}:void 0),a=!1)):s.length||f()):console.error("enableBodyScroll unsuccessful - targetElement must be provided when calling enableBodyScroll on IOS devices.")}},304:(t,e,i)=>{"use strict"
+i.r(e),i.d(e,{default:()=>l,modifier:()=>u})
+const n=require("@ember/application"),o=require("@ember/modifier"),r=require("@ember/destroyable")
+function s(t,e,i){return e in t?Object.defineProperty(t,e,{value:i,enumerable:!0,configurable:!0,writable:!0}):t[e]=i,t}class a{constructor(t){this.owner=t,s(this,"capabilities",(0,o.capabilities)("3.22"))}createModifier(t,e){return{instance:new t(this.owner,e),element:null}}installModifier(t,e,i){const n=function(t,e){const i=t
+return i.element=e,i}(t,e)
+n.instance.modify(e,i.positional,i.named)}updateModifier(t,e){t.instance.modify(t.element,e.positional,e.named)}destroyModifier(t){let{instance:e}=t;(0,r.destroy)(e)}}class l{constructor(t,e){(0,n.setOwner)(this,t)}modify(t,e,i){}}(0,o.setModifierManager)((t=>new a(t)),l)
+const c=new class{constructor(){s(this,"capabilities",(0,o.capabilities)("3.22"))}createModifier(t){return{element:null,instance:t}}installModifier(t,e,i){const n=function(t,e){const i=t
+return i.element=e,i}(t,e),{positional:o,named:r}=i,s=t.instance(e,o,r)
+"function"==typeof s&&(n.teardown=s)}updateModifier(t,e){"function"==typeof t.teardown&&t.teardown()
+const i=t.instance(t.element,e.positional,e.named)
+"function"==typeof i&&(t.teardown=i)}destroyModifier(t){"function"==typeof t.teardown&&t.teardown()}}
+function u(t){return(0,o.setModifierManager)((()=>c),t)}},663:(t,e,i)=>{"use strict"
+function n(t,e){if(!t)throw new Error(e)}function o(t,e){return null!=t?t:e}i.r(e),i.d(e,{Spring:()=>r})
+class r{constructor(){let t=arguments.length>0&&void 0!==arguments[0]?arguments[0]:{}
+this._listeners=[],this._currentAnimationStep=0,this._currentTime=0,this._springTime=0,this._currentValue=0,this._currentVelocity=0,this._isAnimating=!1,this._oscillationVelocityPairs=[],this._config={fromValue:o(t.fromValue,0),toValue:o(t.toValue,1),stiffness:o(t.stiffness,100),damping:o(t.damping,10),mass:o(t.mass,1),initialVelocity:o(t.initialVelocity,0),overshootClamping:o(t.overshootClamping,!1),allowsOverdamping:o(t.allowsOverdamping,!1),restVelocityThreshold:o(t.restVelocityThreshold,.001),restDisplacementThreshold:o(t.restDisplacementThreshold,.001)},this._currentValue=this._config.fromValue,this._currentVelocity=this._config.initialVelocity}start(){const{fromValue:t,toValue:e,initialVelocity:i}=this._config
+return t===e&&0===i||(this._reset(),this._isAnimating=!0,this._currentAnimationStep||(this._notifyListeners("onStart"),this._currentAnimationStep=requestAnimationFrame((t=>{this._step(Date.now())})))),this}stop(){return this._isAnimating?(this._isAnimating=!1,this._notifyListeners("onStop"),this._currentAnimationStep&&(cancelAnimationFrame(this._currentAnimationStep),this._currentAnimationStep=0),this):this}get currentValue(){return this._currentValue}get currentVelocity(){return this._currentVelocity}get isAtRest(){return this._isSpringAtRest()}get isAnimating(){return this._isAnimating}updateConfig(t){this._advanceSpringToTime(Date.now())
+const e={fromValue:this._currentValue,initialVelocity:this._currentVelocity}
+return this._config=Object.assign({},this._config,e,t),this._reset(),this}onStart(t){return this._listeners.push({onStart:t}),this}onUpdate(t){return this._listeners.push({onUpdate:t}),this}onStop(t){return this._listeners.push({onStop:t}),this}removeListener(t){return this._listeners=this._listeners.reduce(((e,i)=>(-1!==Object.values(i).indexOf(t)||e.push(i),e)),[]),this}removeAllListeners(){return this._listeners=[],this}_reset(){this._currentTime=Date.now(),this._springTime=0,this._currentValue=this._config.fromValue,this._currentVelocity=this._config.initialVelocity}_notifyListeners(t){this._listeners.forEach((e=>{const i=e[t]
+"function"==typeof i&&i(this)}))}_step(t){this._advanceSpringToTime(t,!0),this._isAnimating&&(this._currentAnimationStep=requestAnimationFrame((t=>this._step(Date.now()))))}_advanceSpringToTime(t){let e=arguments.length>1&&void 0!==arguments[1]&&arguments[1]
+if(!this._isAnimating)return
+let i=t-this._currentTime
+i>r.MAX_DELTA_TIME_MS&&(i=r.MAX_DELTA_TIME_MS),this._springTime+=i
+const o=this._config.damping,s=this._config.mass,a=this._config.stiffness,l=this._config.fromValue,c=this._config.toValue,u=-this._config.initialVelocity
+n(s>0,"Mass value must be greater than 0"),n(a>0,"Stiffness value must be greater than 0"),n(o>0,"Damping value must be greater than 0")
+let h=o/(2*Math.sqrt(a*s))
+const d=Math.sqrt(a/s)/1e3,f=d*Math.sqrt(1-h*h),m=d*Math.sqrt(h*h-1),_=c-l
+h>1&&!this._config.allowsOverdamping&&(h=1)
+let p=0,g=0
+const v=this._springTime
+if(h<1){const t=Math.exp(-h*d*v)
+p=c-t*((u+h*d*_)/f*Math.sin(f*v)+_*Math.cos(f*v)),g=h*d*t*(Math.sin(f*v)*(u+h*d*_)/f+_*Math.cos(f*v))-t*(Math.cos(f*v)*(u+h*d*_)-f*_*Math.sin(f*v))}else if(1===h){const t=Math.exp(-d*v)
+p=c-t*(_+(u+d*_)*v),g=t*(u*(v*d-1)+v*_*(d*d))}else{const t=Math.exp(-h*d*v)
+p=c-t*((u+h*d*_)*Math.sinh(m*v)+m*_*Math.cosh(m*v))/m,g=t*h*d*(Math.sinh(m*v)*(u+h*d*_)+_*m*Math.cosh(m*v))/m-t*(m*Math.cosh(m*v)*(u+h*d*_)+m*m*_*Math.sinh(m*v))/m}return this._currentTime=t,this._currentValue=p,this._currentVelocity=g,e&&(this._notifyListeners("onUpdate"),this._isAnimating)&&(this._isSpringOvershooting()||this._isSpringAtRest())?(0!==a&&(this._currentValue=c,this._currentVelocity=0,this._notifyListeners("onUpdate")),void this.stop()):void 0}_isSpringOvershooting(){const{stiffness:t,fromValue:e,toValue:i,overshootClamping:n}=this._config
+let o=!1
+return n&&0!==t&&(o=e<i?this._currentValue>i:this._currentValue<i),o}_isSpringAtRest(){const{stiffness:t,toValue:e,restDisplacementThreshold:i,restVelocityThreshold:n}=this._config,o=Math.abs(this._currentVelocity)<=n
+return 0!==t&&Math.abs(e-this._currentValue)<=i&&o}}r.MAX_DELTA_TIME_MS=1/60*1e3*4},315:(t,e,i)=>{var n,o
+t.exports=(n=_eai_d,o=_eai_r,window.emberAutoImportDynamic=function(t){return 1===arguments.length?o("_eai_dyn_"+t):o("_eai_dynt_"+t)(Array.prototype.slice.call(arguments,1))},window.emberAutoImportSync=function(t){return o("_eai_sync_"+t)(Array.prototype.slice.call(arguments,1))},n("body-scroll-lock",[],(function(){return i(863)})),n("ember-modifier",[],(function(){return i(304)})),void n("wobble",[],(function(){return i(663)})))},798:function(t,e){window._eai_r=require,window._eai_d=define}},e={}
+function i(n){var o=e[n]
+if(void 0!==o)return o.exports
+var r=e[n]={exports:{}}
+return t[n].call(r.exports,r,r.exports,i),r.exports}i.d=(t,e)=>{for(var n in e)i.o(e,n)&&!i.o(t,n)&&Object.defineProperty(t,n,{enumerable:!0,get:e[n]})},i.o=(t,e)=>Object.prototype.hasOwnProperty.call(t,e),i.r=t=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},i(798)
+var n=i(315)
+__ember_auto_import__=n})()
